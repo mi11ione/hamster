3a048b68cb9a29a727f80665edc20c3ce5e2470c
Mi11ion <krevetk4444@gmail.com>
2023-01-13 16:43:05 +0500

    Initial Commit

---
+++ jijdfisdufhdisufhuisdhfkuas/jijdfisdufhdisufhuisdhfkuasApp.swift
@@ -0,0 +1,17 @@
+//
+//  jijdfisdufhdisufhuisdhfkuasApp.swift
+//  jijdfisdufhdisufhuisdhfkuas
+//
+//  Created by mi11ion on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct jijdfisdufhdisufhuisdhfkuasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
